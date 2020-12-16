584c6d313ffc6288d5a95e0d33b8cad537dd148b
mahmud-oss <mahmud94pial@gmail.com>
2020-12-17 04:31:55 +0600

    incomplete

--- encryption_transfer/bin/encryption_transfer.dart
+++
@@ -1,5 +0,0 @@
-import 'package:encryption_transfer/encryption_transfer.dart' as encryption_transfer;
-
-void main(List<String> arguments) {
-  print('Hello world: ${encryption_transfer.calculate()}!');
-}
--- encryption_transfer/test/encryption_transfer_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:encryption_transfer/encryption_transfer.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
